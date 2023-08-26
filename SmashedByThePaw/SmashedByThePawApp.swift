76f046d341cdc2429142157aabb41dbf180b951c
Laslo Kozlov <iamlaslo@icloud.com>
2023-08-26 12:37:26 +0300

    Initial Commit

---
+++ SmashedByThePaw/SmashedByThePawApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmashedByThePawApp.swift
+//  SmashedByThePaw
+//
+//  Created by Laslo on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmashedByThePawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
